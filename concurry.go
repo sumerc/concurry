0448be5a702edd0a065433acf16b0fb62d3b1da3
Sumer Cip <sumerc@gmail.com>
2020-04-22 23:04:47 +0300

    add commandTimeout + refactor logging + repeatConcurrency field

--- concurry.go
+++ concurry.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"container/ring"
+	"context"
 	"flag"
 	"fmt"
 	"log"
@@ -14,16 +15,22 @@ import (
 )
 
 type Config struct {
-	//cmd            *string
-	displayOutput *bool
-	verbose       *bool
-	failFast      *bool
-	repeatCount   *uint
+	displayOutput    *bool
+	verbose          *bool
+	failFast         *bool
+	colorize         *bool
+	repeatCount      *uint
+	repeatConcurrent *bool
+	commandTimeout   *uint
 }
+
+// TODO:
+// type ColorRing struct {
+// 	colorReset
+// }
 
 var config Config
 var colors = ring.New(6)
-var colorMutex = &sync.Mutex{}
 var results = []string{}
 var resultsMutex = &sync.Mutex{}
 var colorReset = "\033[0m"
@@ -45,30 +52,46 @@ func initColorRing() {
 	r.Value = "\033[36m" // cyan
 }
 
-func getColor() string {
-	colorMutex.Lock()
-	defer colorMutex.Unlock()
+func GetNextColor() string {
 	colors = colors.Next()
 	return colors.Value.(string)
+}
+
+type TaskLogger struct {
+	taskId int
+	color  string
+}
+
+func (t TaskLogger) Sprintf(format string, args ...interface{}) string {
+	if *config.colorize {
+		format = fmt.Sprintf("%s(Task-%d) %s%s", t.color, t.taskId, format, colorReset)
+	} else {
+		format = fmt.Sprintf("(Task-%d) %s", t.taskId, format)
+	}
+	return fmt.Sprintf(format, args...)
 }
 
 // RunCmd TODO: Comment
 // Note: log.Println() functions are goroutine safe. There is mutex involved when
 // write() is called.
-func RunCmd(command string, wg *sync.WaitGroup) {
+func RunCmd(command string, taskId int, wg *sync.WaitGroup, color string) {
 	defer wg.Done()
 
-	color := getColor()
 	startTime := time.Now()
+	taskLogger := TaskLogger{taskId: taskId, color: color}
 
 	commandArr := strings.Split(command, " ")
 	if *config.verbose {
-		log.Println("Executing ", commandArr)
+		log.Println(taskLogger.Sprintf("Executing '%s'", command))
 	}
-	cmd := exec.Command(commandArr[0], commandArr[1:]...)
+	//cmd := exec.Command(commandArr[0], commandArr[1:]...)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(*config.commandTimeout)*time.Second)
+	defer cancel()
+	cmd := exec.CommandContext(ctx, commandArr[0], commandArr[1:]...)
 
 	stdoutReader, _ := cmd.StdoutPipe()
 	stderrReader, _ := cmd.StderrPipe()
+	// TODO: err handling of above
 	stdoutScanner := bufio.NewScanner(stdoutReader)
 	stderrScanner := bufio.NewScanner(stderrReader)
 	init := make(chan bool)
@@ -78,7 +101,7 @@ func RunCmd(command string, wg *sync.WaitGroup) {
 	go func() {
 		init <- true
 		for stderrScanner.Scan() {
-			log.Println(color, stderrScanner.Text(), colorReset)
+			log.Println(taskLogger.Sprintf(stderrScanner.Text()))
 		}
 	}()
 	<-init
@@ -86,14 +109,18 @@ func RunCmd(command string, wg *sync.WaitGroup) {
 	cmd.Start()
 	if *config.displayOutput {
 		for stdoutScanner.Scan() {
-			log.Println(color, stdoutScanner.Text(), colorReset)
+			log.Println(taskLogger.Sprintf(stdoutScanner.Text()))
 		}
 	}
 	err := cmd.Wait()
 
 	if err != nil {
-		failure := fmt.Sprintf("%s'%s' failed. [%s] [%s] %s", color, command,
-			err, time.Since(startTime), colorReset)
+		errStr := fmt.Sprintf("%s", err)
+		if ctx.Err() == context.DeadlineExceeded {
+			errStr = "Task Timeout"
+		}
+		failure := taskLogger.Sprintf("'%s' failed. [%s] [%s]", command,
+			errStr, time.Since(startTime))
 
 		if *config.failFast {
 			log.Println(failure)
@@ -107,8 +134,8 @@ func RunCmd(command string, wg *sync.WaitGroup) {
 	} else {
 		if *config.verbose {
 			resultsMutex.Lock()
-			results = append(results, fmt.Sprintf("%s'%s' succeeded. [%s] %s", color, command,
-				time.Since(startTime), colorReset))
+			results = append(results, taskLogger.Sprintf("'%s' succeeded. [%s]",
+				command, time.Since(startTime)))
 			resultsMutex.Unlock()
 		}
 	}
@@ -121,13 +148,14 @@ func main() {
 
 	initColorRing()
 
-	// TODO: Ability to set custom shell
-	//config.cmd = flag.String("cmd", "", "command to be run")
-	config.displayOutput = flag.Bool("o", false, "display command output")
+	config.displayOutput = flag.Bool("o", false, "display stdout")
+	//config.bufferIO = flag.Bool("b", false, "buffer stdout/stderr") // TODO
 	config.verbose = flag.Bool("v", true, "show executed command and return values")
 	config.repeatCount = flag.Uint("n", 1, "repeat command N times (synchronously)")
 	config.failFast = flag.Bool("f", true, "fail if any concurrent command fails")
-	//config.waitTimeout
+	config.colorize = flag.Bool("c", true, "colorize the command outputs")
+	config.repeatConcurrent = flag.Bool("rc", false, "run repeated commands concurrently")
+	config.commandTimeout = flag.Uint("t", 60, "timeout for executed command (secs)")
 	flag.Parse()
 
 	reader := bufio.NewReader(os.Stdin)
@@ -143,16 +171,22 @@ func main() {
 		commands = append(commands, command)
 	}
 
+	taskId := 0
 	for i := uint(0); i < *config.repeatCount; i++ {
 		for _, command := range commands {
 			command = strings.TrimSpace(command)
 			if len(command) > 0 {
 				wg.Add(1)
-				go RunCmd(command, &wg)
+				taskId++
+				go RunCmd(command, taskId, &wg, GetNextColor())
 			}
 		}
-		wg.Wait()
+		if !*config.repeatConcurrent {
+			wg.Wait()
+		}
 	}
+
+	wg.Wait()
 
 	// when we come here, all commands finish executing, so it is safe to read
 	// results without a Lock
